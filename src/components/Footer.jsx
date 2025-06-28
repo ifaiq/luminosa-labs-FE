960540c8d88977506992486cf3b11ff64f996d9e
Faiq Shariff <faiq55@yahoo.com>
2025-06-28 22:21:32 +0500

    2121 N. California Blvd Suite 243,                     <br />                     Walnut Creek, CA 94596

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,9 @@ function Footer() {
         <div className="footer-contact">
           <h3>CONTACT US</h3>
           <address>
-            212 North Something,<br />
-            Walnut Creek, CA 94598
+            2121 N. California Blvd Suite 243,
+                    <br />
+                    Walnut Creek, CA 94596
           </address>
         </div>
         
@@ -32,7 +33,7 @@ function Footer() {
         </div>
         
         <div className="footer-social">
-          <a href="https://linkedin.com" className="social-link" target="_blank" rel="noopener">
+          <a href="https://www.linkedin.com/company/luminosa-labs/?viewAsMember=true" className="social-link" target="_blank" rel="noopener">
             <svg className="linkedin-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
             </svg>
